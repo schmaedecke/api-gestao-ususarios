8a34953e18fbdaa871202939d4586353e44bf485
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-07-13 16:46:14 -0300

    modelando banco de dados

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,11 @@
-var knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-      host : '127.0.0.1',
-      user : 'root',
-      password : '',
-      database : 'usertest'
-    }
-  });
+var knex = require("knex")({
+  client: "mysql2",
+  connection: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "G3s9t9v7l8i01!",
+    database: "apiusers",
+  },
+});
 
-module.exports = knex
+module.exports = knex;
